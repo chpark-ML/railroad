6143326f91055347677e128473ab43f612f2d4d0
1pha <1phantasmas@korea.ac.kr>
2023-08-19 23:04:53 +0900

    Minor changes

--- projects/DC_prediction/utils/constants.py
+++ projects/DC_prediction/utils/constants.py
@@ -1,15 +1,13 @@
 import os
-
-from glob import glob
-from enum import Enum
+from pathlib import Path
 
 YAW_TYPES = ['30', '40', '50', '70', '100']
 RAIL_TYPES = ['curved', 'straight']
 
-DATA_ROOT_PATH = '/data/railroad/data'
+DATA_ROOT_PATH = Path('./data')
 
-VIBRATION_DATA_STRAIGHT = glob(os.path.join(DATA_ROOT_PATH, '/data_s**0.csv'))  # [data_s30.csv, ...]
-LANE_DATA_STRAIGHT = os.path.join(DATA_ROOT_PATH, '/lane_data_s.csv')
+VIBRATION_DATA_STRAIGHT = DATA_ROOT_PATH.rglob("*data_s*0.csv")
+LANE_DATA_STRAIGHT = DATA_ROOT_PATH / "lane_data_s.csv"
 
-VIBRATION_DATA_CURVED = glob(os.path.join(DATA_ROOT_PATH, '/data_c**0.csv'))  # [data_c30.csv, ...]
-LANE_DATA_CURVED = os.path.join(DATA_ROOT_PATH, '/lane_data_c.csv')
+VIBRATION_DATA_CURVED = DATA_ROOT_PATH.rglob("*data_c*0.csv")
+LANE_DATA_CURVED = DATA_ROOT_PATH / "lane_data_c.csv"
