e0c63e317c923943f2bfe1a6bd6ff7c5f457aa86
chpark-ML <bch0322@gmail.com>
2023-08-22 05:51:31 +0000

    update

--- projects/DC_prediction/scripts/baseline.sh
+++ projects/DC_prediction/scripts/baseline.sh
@@ -4,12 +4,14 @@ cd /opt/railroad/projects/DC_prediction
 
 epoch=30
 BS=8
+interval=5
 
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.experiment_name=railroad-chpark \
   experiment_tool.run_group=baseline \
-  experiment_tool.run_name=baseline \
+  experiment_tool.run_name=baseline-interva${interval} \
   loader.batch_size=${BS} \
+  loader.dataset.interval=${interval} \
   trainer.max_epoch=${epoch} \
   trainer.gpus=${gpu_num} \
   trainer.fast_dev_run=False
