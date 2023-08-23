6e1f148bffa4e2a0eaf888c16ed3c84574b9204d
chpark-ML <bch0322@gmail.com>
2023-08-23 00:21:48 +0000

    update val analysis

--- projects/DC_prediction/scripts/test.sh
+++ projects/DC_prediction/scripts/test.sh
@@ -2,12 +2,22 @@ gpu_num=$1
 
 cd /opt/railroad/projects/DC_prediction
 
-rail_type=curved
+BS=5
 
 HYDRA_FULL_ERROR=1 python3 analysis/test.py \
   experiment_tool.experiment_name=railroad-chpark \
   experiment_tool.run_group=baseline \
   experiment_tool.run_name=test \
   loader.dataset.rail_type=${rail_type} \
+  loader.batch_size=${BS} \
+  trainer.gpus=${gpu_num} \
+  trainer.fast_dev_run=False
+
+HYDRA_FULL_ERROR=1 python3 analysis/val.py \
+  experiment_tool.experiment_name=railroad-chpark \
+  experiment_tool.run_group=baseline \
+  experiment_tool.run_name=val \
+  loader.dataset.rail_type=${rail_type} \
+  loader.batch_size=${BS} \
   trainer.gpus=${gpu_num} \
   trainer.fast_dev_run=False
